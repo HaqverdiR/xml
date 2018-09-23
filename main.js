0390370c113ec0338da25fab5ace3490315a5b36
Ruslan I. Hagverdi <RuslanIh@CODE.LOCAL>
2018-09-23 20:00:17 +0400

    yeniulik

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 var uN = document.getElementsByName('username')[0];
 var pic = document.getElementsByName('pic')[0];
-
+var progBar = document.getElementById("uploadp");
 
 function clickFunk(){
     var fd = new FormData();
-    var b64 = btoa(pic);
+    // var b64 = btoa(pic);
     fd.append("username", uN.value);
-    fd.append("avatar", b64);
+    fd.append("avatar", pic.files[0]);
     var xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "http://192.168.19.35:3000/user", true);
-    xhttp.setRequestHeader("Content-Type", "application/json");
-    xhttp.send(  JSON.stringify (fd));
-    
+    xhttp.open("POST", "http://192.168.19.35:3000/photo", true);
+    //xhttp.setRequestHeader("Content-Type", "application/x-www-urlencoded");
+     xhttp.upload.onprogress = function(e){
+         console.log(e)
+            var prog = e.loaded / e.total * 100
+         progBar.value = prog
+         
+   
+     }
+     xhttp.send(fd);
 }
 
 
