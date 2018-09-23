9110eca5b0e646080da10f0349b4dfd6e98be962
Hagverdi Ruslan <40520211+HaqverdiR@users.noreply.github.com>
2018-09-23 15:10:20 +0400

    Add files via upload
    yeni

--- main.js
+++ main.js
@@ -2,16 +2,15 @@ var uN = document.getElementsByName('username')[0];
 var pic = document.getElementsByName('pic')[0];
 
 
-
-
 function clickFunk(){
     var fd = new FormData();
+    var b64 = btoa(pic);
     fd.append("username", uN.value);
-    fd.append("avatar", pic.files[0])
-    var rusl = new XMLHttpRequest();
-
-    rusl.open("POST", "https://www.google.com", true);
-    xhttp.send(fd);
+    fd.append("avatar", b64);
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "http://192.168.19.35:3000/user", true);
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.send(  JSON.stringify (fd));
     
 }
 
